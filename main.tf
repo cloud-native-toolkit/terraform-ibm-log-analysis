0ab3bb72b6230b12caed60df383ecc8a481e3f07
Sean Sundberg <seansund@us.ibm.com>
2020-07-19 22:29:09 -0500

    Adds support for multiple modes of operation (#2)
    * Allows the module to be used in one of 4 ways:
        - Provision-only: provision the instance but do not bind to a cluster (set `provision` to `true` and do not provide cluster information)
        - Bind-only: bind the an existing instance to a cluster (set `provision` to `false`, provide the name for the LogDNA instance, and provide the cluster information)
        - Provision-and-bind: provision the instance and bind it to a cluster (set `provision` to `true` and provide cluster information)
        - Do nothing: execute the module but don't create any resources (set `provision` to `false` and don't provide the name of the instance)
    * Replaces `exists` variable with `provision` variable
    * Generates the required service account instead of depending on an external module
    * Generates the tool-config metadata, including the applicationMenu

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
 provider "ibm" {
   version = ">= 1.2.1"
+  region  = var.resource_location
+}
+
+provider "helm" {
+  version = ">= 1.1.1"
+  kubernetes {
+    config_path = var.cluster_config_file_path
+  }
 }
 
 provider "null" {
@@ -11,19 +19,28 @@ data "ibm_resource_group" "tools_resource_group" {
 
 locals {
   name_prefix       = var.name_prefix != "" ? var.name_prefix : var.resource_group_name
-  role              = "Manager"
-  resource_location = var.resource_location
   name              = var.name != "" ? var.name : "${replace(local.name_prefix, "/[^a-zA-Z0-9_\\-\\.]/", "")}-logdna"
+  role              = "Manager"
+  provision         = var.provision
+  bind              = (var.provision || (!var.provision && var.name != "")) && var.cluster_config_file_path != "" && var.cluster_type != ""
+  image_url         = var.base_icon_url != "" ? "${var.base_icon_url}/logdna" : ""
+}
+
+resource "null_resource" "logging" {
+  provisioner "local-exec" {
+    command = "echo 'provision: ${local.provision}, bind: ${local.bind}'"
+  }
 }
 
 // LogDNA - Logging
 resource "ibm_resource_instance" "logdna_instance" {
-  count             = var.exists ? 0 : 1
+  depends_on = [null_resource.logging]
+  count             = local.provision ? 1 : 0
 
   name              = local.name
   service           = "logdna"
   plan              = var.plan
-  location          = local.resource_location
+  location          = var.resource_location
   resource_group_id = data.ibm_resource_group.tools_resource_group.id
   tags              = var.tags
 
@@ -35,17 +52,20 @@ resource "ibm_resource_instance" "logdna_instance" {
 }
 
 data "ibm_resource_instance" "logdna_instance" {
+  count             = local.bind ? 1 : 0
   depends_on        = [ibm_resource_instance.logdna_instance]
 
   name              = local.name
   resource_group_id = data.ibm_resource_group.tools_resource_group.id
-  location          = local.resource_location
+  location          = var.resource_location
   service           = "logdna"
 }
 
 resource "ibm_resource_key" "logdna_instance_key" {
-  name                 = "${data.ibm_resource_instance.logdna_instance.name}-key"
-  resource_instance_id = data.ibm_resource_instance.logdna_instance.id
+  count = local.bind ? 1 : 0
+
+  name                 = "${local.name}-key"
+  resource_instance_id = data.ibm_resource_instance.logdna_instance[0].id
   role                 = local.role
 
   //User can increase timeouts 
@@ -55,14 +75,64 @@ resource "ibm_resource_key" "logdna_instance_key" {
   }
 }
 
+resource "null_resource" "scc-cleanup" {
+  count = local.bind ? 1 : 0
+
+  provisioner "local-exec" {
+    command = "kubectl delete scc -l app.kubernetes.io/name=${var.service_account_name} --wait 1> /dev/null 2> /dev/null || true"
+
+    environment = {
+      KUBECONFIG = var.cluster_config_file_path
+    }
+  }
+}
+
+resource "helm_release" "service-account" {
+  count = local.bind ? 1 : 0
+  depends_on = [null_resource.scc-cleanup]
+
+  name              = var.service_account_name
+  chart             = "service-account"
+  namespace         = var.namespace
+  repository        = "https://ibm-garage-cloud.github.io/toolkit-charts/"
+  timeout           = 1200
+  force_update      = true
+  replace           = true
+
+  disable_openapi_validation = true
+
+  set {
+    name  = "global.clusterType"
+    value = var.cluster_type
+  }
+
+  set {
+    name  = "name"
+    value = var.service_account_name
+  }
+
+  set {
+    name  = "create"
+    value = true
+  }
+
+  set {
+    name  = "sccs"
+    value = "{anyuid, privileged}"
+  }
+}
+
 resource "null_resource" "logdna_bind" {
+  count = local.bind ? 1 : 0
+  depends_on = [helm_release.service-account]
+
   triggers = {
     namespace  = var.namespace
     KUBECONFIG = var.cluster_config_file_path
   }
 
   provisioner "local-exec" {
-    command = "${path.module}/scripts/bind-logdna.sh ${var.cluster_type} ${ibm_resource_key.logdna_instance_key.credentials.ingestion_key} ${local.resource_location} ${var.namespace} ${var.service_account_name}"
+    command = "${path.module}/scripts/bind-logdna.sh ${var.cluster_type} ${ibm_resource_key.logdna_instance_key[0].credentials.ingestion_key} ${var.resource_location} ${var.namespace} ${var.service_account_name}"
 
     environment = {
       KUBECONFIG = self.triggers.KUBECONFIG
@@ -77,5 +147,57 @@ resource "null_resource" "logdna_bind" {
     environment = {
       KUBECONFIG = self.triggers.KUBECONFIG
     }
+  }
+}
+
+resource "null_resource" "delete-consolelink" {
+  count = var.cluster_type == "ocp4" && local.bind ? 1 : 0
+
+  provisioner "local-exec" {
+    command = "kubectl delete consolelink -l grouping=garage-cloud-native-toolkit -l app=logdna || exit 0"
+
+    environment = {
+      KUBECONFIG = var.cluster_config_file_path
+    }
+  }
+}
+
+resource "helm_release" "logdna" {
+  count = local.bind ? 1 : 0
+  depends_on = [null_resource.logdna_bind]
+
+  name              = "logdna"
+  chart             = "tool-config"
+  namespace         = var.namespace
+  repository        = "https://ibm-garage-cloud.github.io/toolkit-charts/"
+  timeout           = 1200
+  force_update      = true
+  replace           = true
+
+  disable_openapi_validation = true
+
+  set {
+    name  = "displayName"
+    value = "LogDNA"
+  }
+
+  set {
+    name  = "url"
+    value = "https://cloud.ibm.com/observe/logging"
+  }
+
+  set {
+    name  = "imageUrl"
+    value = local.image_url
+  }
+
+  set {
+    name  = "applicationMenu"
+    value = true
+  }
+
+  set {
+    name  = "global.clusterType"
+    value = var.cluster_type
   }
 }
