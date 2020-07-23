bbf8ff6ae913316e3c4866ddad45d027ddd53fde
Sean Sundberg <seansund@us.ibm.com>
2020-07-23 14:38:20 -0500

    Integrated bind logic (#3)
    * Updates logic to install the agent via the `ibmcloud ob` plugin
    * Updates bind logic to check for existing config and remove it if necessary
    * Renames bind scripts to be more generic
    * Use instance name instead of instance id to bind to cluster

---
+++ scripts/bind-instance.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd -P)
+MODULE_DIR=$(cd ${SCRIPT_DIR}/..; pwd -P)
+
+CLUSTER_ID="$1"
+INSTANCE_NAME="$2"
+INGESTION_KEY="$3"
+
+echo "Configuring LogDNA for ${CLUSTER_ID} cluster and ${INSTANCE_NAME} LogDNA instance"
+
+if ibmcloud ob logging config ls --cluster "${CLUSTER_ID}" | grep -q "Instance name"; then
+  EXISTING_INSTANCE_NAME=$(ibmcloud ob logging config ls --cluster "${CLUSTER_ID}" | grep "Instance name" | sed -E "s/Instance name: +([^ ]+)/\1/g")
+  if [[ "${EXISTING_INSTANCE_NAME}" == "${INSTANCE_NAME}" ]]; then
+    echo "LogDNA configuration already exists on this cluster"
+    exit 0
+  else
+    echo "Existing LogDNA configuration found on this cluster for a different LogDNA instance: ${EXISTING_INSTANCE_NAME}."
+    echo "Removing the config before creating the new one"
+    ibmcloud ob logging config delete \
+      --cluster "${CLUSTER_ID}" \
+      --instance "${EXISTING_INSTANCE_NAME}" \
+      --force
+  fi
+fi
+
+set -e
+
+echo "Creating LogDNA configuration for ${CLUSTER_ID} cluster and ${INSTANCE_NAME} LogDNA instance"
+ibmcloud ob logging config create \
+  --cluster "${CLUSTER_ID}" \
+  --instance "${INSTANCE_NAME}" \
+  --logdna-ingestion-key "${INGESTION_KEY}"
