da2efbc7f15aaa838baeb9a00855272bdb48fa9b
Sean Sundberg <seansund@us.ibm.com>
2020-08-19 15:54:57 -0500

    Adds cluster_config_file_path value from cluster

--- test/stages/stage-logdna.tf
+++ test/stages/stage-logdna.tf
@@ -7,6 +7,7 @@ module "dev_logdna" {
   cluster_id               = module.dev_cluster.id
   cluster_name             = module.dev_cluster.name
   cluster_type             = module.dev_cluster.type_code
+  cluster_config_file_path = module.dev_cluster.config_file_path
   tools_namespace          = module.dev_capture_state.namespace
   name_prefix              = var.name_prefix
 }
