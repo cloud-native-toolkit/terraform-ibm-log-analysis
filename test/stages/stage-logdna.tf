bd4aedb52863805ae3bdd634e08ad3df16c2039c
Sean Sundberg <seansund@us.ibm.com>
2021-03-30 23:26:30 -0500

    Updates test to remove cluster_type
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage-logdna.tf
+++ test/stages/stage-logdna.tf
@@ -6,7 +6,6 @@ module "dev_logdna" {
   provision                = true
   cluster_id               = module.dev_cluster.id
   cluster_name             = module.dev_cluster.name
-  cluster_type             = module.dev_cluster.type_code
   cluster_config_file_path = module.dev_cluster.config_file_path
   tools_namespace          = module.dev_capture_state.namespace
   name_prefix              = var.name_prefix
