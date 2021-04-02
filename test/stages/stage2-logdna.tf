c51296b0a0d1d25a45e4b015f5b4569998e359de
Sean Sundberg <seansund@us.ibm.com>
2021-04-02 00:15:21 -0500

    Adds resource group dependency
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-logdna.tf
+++ test/stages/stage2-logdna.tf
@@ -1,7 +1,7 @@
 module "logdna" {
   source = "./module"
 
-  resource_group_name      = var.resource_group_name
+  resource_group_name      = module.resource_group.name
   region                   = var.region
   provision                = true
   name_prefix              = var.name_prefix
